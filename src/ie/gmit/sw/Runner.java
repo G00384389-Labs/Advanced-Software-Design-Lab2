f3ee4e0e22f57e247c819396cb8c44ad568650e7
Mark <g00384389@atu.ie>
2024-10-01 14:28:51 +0000

    First Commit as far as running cypher for first time

---
+++ src/ie/gmit/sw/Runner.java
@@ -0,0 +1,13 @@
+package ie.gmit.sw;
+
+public class Runner {
+
+		public static void main(String[] args) throws Throwable{
+			RSACypher cypher = new RSACypher();
+			
+		
+			byte[] s = cypher.encrypt(new String("Attack the castle wall at dawn").getBytes("UTF-8"));		
+			System.out.println(new String(s));
+			System.out.println(new String(cypher.decrypt(s)).getBytes("UTF-8"));
+		}
+}
