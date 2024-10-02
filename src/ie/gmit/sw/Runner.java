2bf279c69e06e2a6f9f4bdbf1b2372224061a589
Mark <g00384389@atu.ie>
2024-10-02 14:26:25 +0000

    Deleted AESCypher, all put into SymmetricCypher, which is no longer Abstract

--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -3,7 +3,8 @@ package ie.gmit.sw;
 public class Runner {
 
 		public static void main(String[] args) throws Throwable{
-			AbstractCypher cypher = new AESCypher();
+//			AbstractCypher cypher = new SymmetricCypher("AES",128,"AES/ECB/PKCS5Padding");
+			AbstractCypher cypher = new SymmetricCypher("DES",56,"DES/ECB/PKCS5Padding");
 			
 		
 			byte[] s = cypher.encrypt(new String("Attack the castle wall at dawn").getBytes("UTF-8"));		
